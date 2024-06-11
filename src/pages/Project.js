be9627d05ae6303b4ef7d4e600969b13e39b458d
jolmcn <jolmcn@gmail.com>
2024-06-11 14:58:57 +0100

    request updates

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,16 +3,32 @@ import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
 import Markdown from "react-markdown";
 import { NavLink } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
-
+// import useFetch from "../hooks/useFetch";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const Project = () => {
   const { id } = useParams();
-  const {loading, error, data} = useFetch("https://strapi-production-07a4.up.railway.app://api/projects/" + id + "?populate=*");
+  // const {loading, error, data} = useFetch("https://strapi-production-07a4.up.railway.app://api/projects/" + id + "?populate=*");
   
+  const [project, setProject] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://strapi-production-07a4.up.railway.app/api/projects/" + id + "?populate=*")
+      .then(({ data }) => setProject(data.data))
+      .then(() => setLoading(false))
+      .catch((error) => setError(error));
+  });
+
+
   if(loading) return <p>Loading...</p>
   if(error) return <p>Error</p>
-  // if(data) return <p>{data.data.data.attributes.Title}</p>
-  const thumbnailAddress = "https://strapi-production-07a4.up.railway.app:" + data.attributes.thumbnail.data.attributes.url;
+
+
+  const thumbnailAddress = project.attributes.ProjectThumb.data.attributes.formats.large.url;
+  
   return ( 
     <>
       <Navigation />
@@ -22,11 +38,14 @@ const Project = () => {
         </div>
         <div className="projectHero" style={{backgroundImage: `url(${thumbnailAddress})`}}></div>
         <div className="projectSubContent">
-          <h2 className="projectDetails"><span className="projectCompany">{data.attributes.ProjectCompany}</span>  <span className="projectDate">{data.attributes.Date.substring(0,4)}</span></h2>
-          <h1 className="projectTitle">{data.attributes.ProjectTitle}</h1>
-          {/* <h3 className="projectDate">—{data.attributes.Date.substring(0,4)}</h3> */}
+          <h2 className="projectDetails">
+            <span className="projectCompany">{project.attributes.ProjectCompany}</span>  
+            <span className="projectDate">{project.attributes.ProjectDate.substring(0,4)}</span>
+          </h2>
+          <h1 className="projectTitle">{project.attributes.ProjectTitle}</h1>
+          {/* <h3 className="projectDate">—{data.attributes.ProjectDate.substring(0,4)}</h3> */}
           <Markdown className="projectContent">
-            {data.attributes.ProjectContent}
+            {project.attributes.ProjectContent}
           </Markdown>
         </div>
       </div>
